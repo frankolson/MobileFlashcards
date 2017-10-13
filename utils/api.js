7bf8b2e60e31b3632d80830c8ffa21c48c685046
Will Olson <will.olson11@gmail.com>
2017-10-13 14:20:23 -0700

    feat: Add initial api functions

---
+++ utils/api.js
@@ -0,0 +1,30 @@
+import { AsyncStorage } from 'react-native';
+import uuidv4 from 'uuid/v4';
+
+const FLASHCARD_STORAGE_KEY = 'MobileFlashcards:data';
+
+export const getDecks = () => (
+  AsyncStorage.getItem(FLASHCARD_STORAGE_KEY)
+);
+
+export const getDeck = key => (
+  getDecks().then(results => results[key])
+);
+
+export const saveDeckTitle = title => (
+  AsyncStorage.mergeItem(FLASHCARD_STORAGE_KEY, JSON.stringify({
+    [uuidv4()]: {
+      title,
+      questions: [],
+    },
+  }))
+);
+
+export const addCardToDeck = (id, card) => {
+  const deck = {...getDeck(id)};
+  deck.questions.push(card);
+
+  return AsyncStorage.mergeItem(FLASHCARD_STORAGE_KEY, JSON.stringify({
+    [id]: { ...deck },
+  }));
+};
