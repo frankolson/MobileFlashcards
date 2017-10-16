a0cc9d7cbf5cecbfda9d6690b576ac07895eb643
Will Olson <will.olson11@gmail.com>
2017-10-15 18:32:05 -0700

    feat: Hook up redux

--- App.js
+++ App.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import { View, StatusBar, StyleSheet } from 'react-native';
 import { TabNavigator } from 'react-navigation';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Constants } from 'expo';
 
 // Project Assets
 import Decks from './components/Decks';
 import NewDeck from './components/NewDeck';
+import reducers from './reducers';
 
 const FlashcardStatusBar = props => (
   <View style={{ height: Constants.statusBarHeight }}>
@@ -33,10 +36,12 @@ const styles = StyleSheet.create({
 });
 
 const App = () => (
-  <View style={styles.container}>
-    <FlashcardStatusBar barStyle="light-content" />
-    <Tabs />
-  </View>
+  <Provider store={createStore(reducers)}>
+    <View style={styles.container}>
+      <FlashcardStatusBar barStyle="light-content" />
+      <Tabs />
+    </View>
+  </Provider>
 );
 
 export default App;
---
+++ reducers/index.js
@@ -0,0 +1,5 @@
+function score(state = {}, action) {
+  return state;
+}
+
+export default score;
