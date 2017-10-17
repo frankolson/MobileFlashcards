e170ae437912dd9309a61758464775379f9b6bc1
Will Olson <will.olson11@gmail.com>
2017-10-16 20:09:43 -0700

    fix: dispatch receiveDecks

--- containers/NewDeckContainer.js
+++ containers/NewDeckContainer.js
@@ -6,9 +6,9 @@ import NewDeck from '../components/NewDeck';
 import { receiveDecks } from '../actions';
 import { getDecks, saveDeckTitle } from '../utils/api';
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = dispatch => ({
   getDecks,
-  receiveDecks,
+  receiveDecks: data => dispatch(receiveDecks(data)),
   saveDeckTitle,
 });
 
