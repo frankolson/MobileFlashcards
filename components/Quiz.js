c840534afac5befbff2385eeb11391e4d3c1045d
Will Olson <will.olson11@gmail.com>
2017-10-22 15:58:09 -0700

    feat: Add basic quiz component

---
+++ components/Quiz.js
@@ -0,0 +1,12 @@
+// Vendor Assets
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// Project Assets
+const Quiz = () => (
+  <View>
+    <Text>Quiz time</Text>
+  </View>
+);
+
+export default Quiz;
