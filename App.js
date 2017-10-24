53191c63ee303f37019e0ea44188f38e22cd2850
Will Olson <will.olson11@gmail.com>
2017-10-23 18:26:48 -0700

    feat: Style the headers

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Constants } from 'expo';
 import devToolsEnhancer from 'remote-redux-devtools';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 // Project Assets
 import DecksContainer from './containers/DecksContainer';
@@ -14,50 +15,78 @@ import NewCardContainer from './containers/NewCardContainer';
 import NewDeckContainer from './containers/NewDeckContainer';
 import QuizContainer from './containers/QuizContainer';
 import reducers from './reducers';
-import { lightGrey } from './utils/colors';
+import { lightGrey, primary, white } from './utils/colors';
 
-const FlashcardStatusBar = props => (
-  <View style={{ height: Constants.statusBarHeight }}>
-    <StatusBar translucent {...props} />
+const FlashcardStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+    <StatusBar backgroundColor={backgroundColor} translucent {...props} />
   </View>
 );
 
 const Tabs = TabNavigator({
   Decks: {
     screen: DecksContainer,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => <Ionicons name="ios-bookmarks" size={30} color={tintColor} />,
+    },
   },
   NewDeck: {
     screen: NewDeckContainer,
     navigationOptions: {
       tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name="plus-square" size={30} color={tintColor} />,
     },
   },
 }, {
   navigationOptions: {
     header: null,
   },
+  tabBarOptions: {
+    activeTintColor: primary,
+    style: {
+      backgroundColor: white,
+    },
+  },
 });
 
 const MainNavigator = StackNavigator({
   Home: {
     screen: Tabs,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: primary,
+      },
+    },
   },
   DeckMenu: {
     screen: DeckMenuContainer,
     navigationOptions: ({ navigation }) => ({
       title: navigation.state.params.deck.title,
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: primary,
+      },
     }),
   },
   NewCard: {
     screen: NewCardContainer,
     navigationOptions: ({ navigation }) => ({
       title: `New Card: ${navigation.state.params.deck.title}`,
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: primary,
+      },
     }),
   },
   Quiz: {
     screen: QuizContainer,
     navigationOptions: ({ navigation }) => ({
       title: `Quiz: ${navigation.state.params.deck.title}`,
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: primary,
+      },
     }),
   },
 });
@@ -72,7 +101,7 @@ const styles = StyleSheet.create({
 const App = () => (
   <Provider store={createStore(reducers, devToolsEnhancer())}>
     <View style={styles.container}>
-      <FlashcardStatusBar barStyle="light-content" />
+      <FlashcardStatusBar backgroundColor={primary} barStyle="light-content" />
       <MainNavigator />
     </View>
   </Provider>
--- utils/colors.js
+++ utils/colors.js
@@ -1,3 +1,10 @@
+export const primary = '#0275d8';
+export const success = '#5cb85c';
+export const info = '#5bc0de';
+export const warning = '#f0ad4e';
+export const danger = '#d9534f';
+
+
 export const black = '#000000';
 export const grey = '#808080';
 export const lightGrey = '#fafafa';
